9b0c9152a65a693f4823d70e0bf2d69dc8b52dc9
Enrico Risa <enrico.risa@gmail.com>
2023-11-30 22:48:45 +0100

    chore: fix axum example

--- axum/axum-basic/src/bin/main.rs
+++ axum/axum-basic/src/bin/main.rs
@@ -6,7 +6,8 @@ use miwa_axum::extensions::web_service_extension;
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
 
-    System::prepare()?
+    System::prepare()
+        .build()?
         .add_extension(web_service_extension)
         .add_extension(todo_api_extension)
         .add_extension(todo_repo_extension)
