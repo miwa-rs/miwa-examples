ad9bfef8384d3ffb453aab8622a9d2cafd0d707d
Enrico Risa <enrico.risa@gmail.com>
2023-11-30 08:32:35 +0100

    wip

--- axum/axum-basic/src/bin/main.rs
+++ axum/axum-basic/src/bin/main.rs
@@ -1,11 +1,15 @@
 use miwa::core::{Extension, System, SystemContext, SystemResult};
 use miwa::derive::{extension, Injectable};
+use miwa_axum::extensions::web_service_extension;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
 
-    System::prepare()?.start().await?;
+    System::prepare()?
+        .add_extension(web_service_extension)
+        .start()
+        .await?;
 
     Ok(())
 }
